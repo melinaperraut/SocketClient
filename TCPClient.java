973232cb44915caf67b187f8d2159d99748c2b03
Melina Perraut <melina@perraut.us>
2023-04-09 20:35:23 -0700

    Clean up code

--- TCPClient.java
+++ TCPClient.java
@@ -7,7 +7,6 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 // Java TCPClient host port
-// Note: prof referred to Java 20 documentation (Oracle)
 public class TCPClient {
 	public static void main(String... args) { // accepts argument parameters from command line
 		String host = args[0]; // define first parameter
@@ -19,17 +18,15 @@ public class TCPClient {
             hasThirdParameter = true;
         }
 		
-		try { // COULD ALSO DO: try (Socket sock = new Socket(host, port)) {... -> closes socket automatically??
-			Socket sock = new Socket(host, port); // create new socket - should put socket into open connection -> if not, Java signals via exception & try/catch block
-
+		try {
+			Socket sock = new Socket(host, port); // create new socket (should put socket into open connection, otherwise signals via exception)
             OutputStream out = sock.getOutputStream(); // get output stream from socket (only accepts bytes by default)
-            // out.write(args[2].getBytes()); // takes 3rd argument parameter, converts to bytes, writes to outputstream
 
             if (hasThirdParameter) {
                 OutputStreamWriter streamWriter = new OutputStreamWriter(out);
-                streamWriter.append(command); // writes the string to the output stream
+                streamWriter.append(command); // writes string to output stream
                 streamWriter.append("\n\n"); // adds end of line characters    
-                streamWriter.flush(); // flushes output stream (really sends everything that was appended before)  
+                streamWriter.flush(); // flushes output stream (sends everything that was appended before)  
             }
 
             InputStream in = sock.getInputStream(); // get input stream from socket
@@ -43,10 +40,6 @@ public class TCPClient {
                 System.out.println(text);
             }
 
-			// int readChar = 0;
-			// while ((readChar = in.read()) != -1) {
-			// 	System.out.write(readChar);
-			// }
 			sock.close();
 		}
 		catch (IOException ex) { // catch exceptions
@@ -55,37 +48,3 @@ public class TCPClient {
 		
 	}
 }
-
-
-// CODE FROM CLASS (LECTURE 4):
-/* 
-import java.net.*;
-
-// Java TCPClient host port
-// Note: prof referred to Java 20 documentation (Oracle)
-public class TCPClient {
-	public static void main(String... args) {
-		String host = args[0];
-		int port = Integer.valueOf(args[1]);
-		
-		try (Socket sock = new Socket(host, port)) { // closes socket automatically??
-			OutputStream out = sock.getOutputStream();
-			String get = "GET / HTTP/1.0\n\n";
-			out.write(get.getBytes());
-			
-			//Socket sock = new Socket(host, port); // should put socket into open connection -> if not, Java signals so via exception & try/catch block
-			InputStream in = sock.getInputStream(); 
-			int readChar = 0;
-			while ((readChar = in.read()) != -1) {
-				System.out.write(readChar);
-			}
-			
-			// sock.close();
-		}
-		catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		
-	}
-}
-*/
